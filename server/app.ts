c02b212658064d7f9aad5db2001c9ec0840578b7
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2025-07-14 14:23:30 +0200

    feat: :zap: server auth added

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { notesRoute } from "./routes/notes";
 import { serveStatic } from "hono/bun";
+import { authRoute } from "./routes/auth";
 
 const app = new Hono();
 
@@ -11,7 +12,10 @@ app.get("/test", (c) => {
   return c.json({ message: "Hello, Hono!" });
 });
 
-const apiRoutes = app.basePath("/api").route("/notes", notesRoute);
+const apiRoutes = app
+  .basePath("/api")
+  .route("/notes", notesRoute)
+  .route("/", authRoute);
 
 app.use("*", serveStatic({ root: "./frontend/dist" }));
 app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
