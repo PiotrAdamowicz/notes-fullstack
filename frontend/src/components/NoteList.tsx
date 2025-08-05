e05ff26316e8298492da485509d54285b5578a75
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2025-08-05 18:44:43 +0200

    feat: :lipstick: styling notes lsit grid

--- frontend/src/components/NoteList.tsx
+++ frontend/src/components/NoteList.tsx
@@ -12,11 +12,11 @@ export default function NoteList({ isPending, error, notes }: NoteListProps) {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className="flex gap-4">
+    <div className="sm:max-w-4/5 mx-auto grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5">
       {isPending
         ? "..."
         : notes.map((note) => (
-            <Card key={note.id}>
+            <Card className="" key={note.id}>
               <CardHeader>
                 <CardTitle>{note.title}</CardTitle>
               </CardHeader>
